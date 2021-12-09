46cb380bf5e0b9892bc3183aea063701b61be3f1
DMITRIY YUSHIN <49056603+DmitrySar@users.noreply.github.com>
2021-12-09 15:50:39 +0300

    bugfix

--- src/main/resources/static/js/dynamicAddFields.js
+++ src/main/resources/static/js/dynamicAddFields.js
@@ -1,16 +1,16 @@
 let ids = 0;
 let idr = 0;
-function addField() {
+function addField(id) {
     let vs = [];
     let vr = [];
-    elem = document.getElementById("dynamic");
+    elem = document.getElementById(id);
     for (i = 0; i < ids; i++) {
         vs.push(document.getElementById('ids'+i).value);
         vr.push(document.getElementById('idr'+i).value);
     }
-    elem.innerHTML += `<br><input placeholder="step" type="text" name="stepNames" id="ids${ids++}">`;
-    elem.innerHTML += `<input placeholder="expRes" type="text" name="stepRes" id="idr${idr++}">`
-    elem.innerHTML += "<button onclick=\"addField()\" type=\"button\">+</button>"
+    elem.innerHTML += `<br><input placeholder="действия" type="text" name="stepNames" id="ids${ids++}">`;
+    elem.innerHTML += `<input placeholder="ожидаемый результат" type="text" name="stepRes" id="idr${idr++}">`
+    elem.innerHTML += `<button onclick=\"addField('${id}')\" type=\"button\">+</button>`
     for (i = 0; i < ids - 1; i++) {
         document.getElementById('ids'+i).value = vs[i];
         document.getElementById('idr'+i).value = vr[i];
