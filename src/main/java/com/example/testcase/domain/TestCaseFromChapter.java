9d89774b44d9a5cc413259a08dea321d23fffca3
DMITRIY YUSHIN <49056603+DmitrySar@users.noreply.github.com>
2021-12-14 13:12:24 +0300

    create TestCaseFromChapter

---
+++ src/main/java/com/example/testcase/domain/TestCaseFromChapter.java
@@ -0,0 +1,11 @@
+package com.example.testcase.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TestCaseFromChapter {
+    private int chapterId;
+    private int testCaseId;
+}
