fdf22609fa5d03678e78b787c45126e5ebee291b
DMITRIY YUSHIN <49056603+DmitrySar@users.noreply.github.com>
2022-08-04 16:13:43 +0300

    remove annotations

--- src/main/java/com/example/testcase/domain/TestCases.java
+++ src/main/java/com/example/testcase/domain/TestCases.java
@@ -1,19 +1,12 @@
 package com.example.testcase.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 public class TestCases {
 
-    @Value("testCases")
     private List<TestCase> testCases;
-
-    @Autowired
-
 
     public TestCases() {
     }
